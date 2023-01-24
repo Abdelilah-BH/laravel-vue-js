9cb1b6096d7a923a12761cb18d523f385af823a5
kettaoui-ikram <kettaouiikram@gmail.com>
2023-01-24 17:36:16 +0100

    fix routes

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,24 +2,25 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Products from "../pages/product/index.vue";
 import Home from "../pages/home.vue";
-// import NotFound from "../pages/not-found.vue";
+import NotFound from "../pages/not-found.vue";
 
 const routes = [
+    {
+        path: "/product",
+        name: "Product",
+        component: Products,
+    },
     {
         path: "/",
         name: "Home",
         component: Home,
     },
+
     {
-        path: "/products",
-        name: "Products",
-        component: Products,
+        path: "/:pathMatch(.*)*",
+        name: "NotFound",
+        component: NotFound,
     },
-    // {
-    //     path: "/",
-    //     name: "NotFound",
-    //     component: NotFound,
-    // },
 ];
 
 const router = createRouter({
